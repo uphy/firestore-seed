be96b119f54d49aa1e35f0d8e078338b1e269f4d
Yuhi Ishikura <yuhi.ishikura@gmail.com>
2020-01-04 22:28:33 +0900

    Updated dependencies of examples.

--- set-version.sh
+++ set-version.sh
@@ -2,18 +2,26 @@
 
 if [ $# != 1 ]; then
   echo Specify version.
+  echo "Usage: ./set-version 0.0.8"
+  echo "       INSTALL=1 ./set-version 0.0.8"
   exit 1
 fi
 VERSION=$1
+INSTALL=${INSTALL:=0}
 
 function update_version(){
     local file=$1
     sed -i "" -e "s/firestore-seed\": \".*\"/firestore-seed\": \"^$VERSION\"/" "$file"
-    pushd . > /dev/null
-    yarn install
+    if [ $INSTALL == 1 ]; then
+      pushd . > /dev/null
+      cd "$(dirname $file)"
+      yarn install
+      popd > /dev/null
+    fi
 }
 
 export -f update_version
 export VERSION
+export INSTALL
 find examples -maxdepth 2 -name "package.json" | xargs -I% sh -c 'update_version "%"'
 sed -i "" -e "s/\"version\": \".*\",/\"version\": \"$VERSION\",/" package.json
